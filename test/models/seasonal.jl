1e3c23ccdb010b9fad3cf312d4d4f8fa8374fc8a
Sebastian Callh <sebastian.callh@gmail.com>
2022-07-29 20:22:42 +0200

    Comment seasonal  test

--- test/models/seasonal.jl
+++ test/models/seasonal.jl
@@ -6,10 +6,10 @@
     x = collect(1:4)
     
     y₁, x₁, ϵ₁ = m(x, 1)
-    @test only(y₁) == x[1]
-    @test all(x₁ .== x[[2, 3, 4, 1]])
-    @test all(ϵ₁ .== vcat(drift_scale, zeros(num_seasons-1)))
+    @test only(y₁) == x[1] # observe first effect
+    @test all(x₁ .== x[[2, 3, 4, 1]]) # cycle seasons
+    @test all(ϵ₁ .== vcat(drift_scale, zeros(num_seasons-1))) # new season => drift scale
     
     y₂, x₂, ϵ₂ = m(x, 2)
-    @test all(ϵ₂ .== 0)
+    @test all(ϵ₂ .== 0) # no new season => no drift scale
 end
