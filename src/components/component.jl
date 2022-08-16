840cc812b916b7dbd398326e5d509cf9fa76b433
Sebastian Callh <sebastian.callh@gmail.com>
2022-08-16 22:59:54 +0200

    Fix off-by-one issue simulating seasonal

--- src/components/component.jl
+++ src/components/component.jl
@@ -8,20 +8,21 @@ Base.:(+)(c1::Component{T}, c2::Component{T}) where T = Sum{T}([c1, c2], reduce(
 Simulates from the provided `model` for `steps` time steps
 with initial state mean `x` with covariance matrix `P` and observation noise `σ`.
 """
-function simulate(sts::Component, steps, x, P, σ; jitter=1e-8)
+function simulate(sts, steps, x₀, P₀, σ; jitter=1e-8)
     R = [σ;;]
-    T = eltype(x)
+    T = eltype(x₀)
     latent_dim = latent_size(sts)
     obs_dim = size(R, 1)
-    xs = Matrix{T}(undef, latent_dim, steps + 1)
-    ys = Matrix{T}(undef, obs_dim, steps + 1)
-    xs[:, 1] = x
-    for t in 2:steps
-        x, P = transition(sts, xs[:,t-1], P, t)
+    xs = Matrix{T}(undef, latent_dim, steps)
+    ys = Matrix{T}(undef, obs_dim, steps)
+    xs[:, 1] = x₀
+    x, P = copy(x₀), copy(P₀)
+    for t in 1:size(xs, 2)
+        x, P = transition(sts, xs[:,max(t-1, 1)], P, t)
         y, S = observe(sts, x, P, R)
         xs[:,t] = rand(Gaussian(x, P .+ jitter .* abs.(diagm(randn(latent_dim)))))
         ys[:,t] = rand(Gaussian(y, S))
     end
 
-    return xs[:,2:end], ys[:,2:end]
+    return xs, ys
 end
